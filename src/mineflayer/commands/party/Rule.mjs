cae9f74da3e9924d70cd0a43f2e82256b3900076
unterrunden <133593594+unterrunden@users.noreply.github.com>
2025-06-10 21:18:09 +0200

    Update Rule.mjs
    - separated 'user' from 'splasher' rules
    - normal (user) rules can be accessed as before with !p rule [number], or through the alias !p r [number]
    - splasher rules can be accessed with !p rule splasher (or 's') [number], or through the alias !p r(ule) s [number]
    - splasher rules won't be shown in party chat, but instead as a whisper to the user

--- src/mineflayer/commands/party/Rule.mjs
+++ src/mineflayer/commands/party/Rule.mjs
@@ -5,9 +5,9 @@ let lastRuleSentTime = 0;
 const COOLDOWN_DURATION = 5_000;
 
 export default {
-  name: ["rule"],
-  description: "Send a BingoBrewers rule in party chat",
-  usage: "!p rule [number]",
+  name: ["rule", "r"],
+  description: "Send a BingoBrewers rule in party chat or a Splasher rule in whispers.",
+  usage: "!p rule [splasher] [number]",
   permission: Permissions.ExSplasher,
 
   /**
@@ -26,15 +26,45 @@ export default {
 
     lastRuleSentTime = currentTime;
 
-    let rule = bot.utils.rulesList[args[0]] ?? bot.utils.rulesList["1"];
-    let ruleNum = Object.keys(bot.utils.rulesList).find(
-      (key) => bot.utils.rulesList[key] === rule,
-    );
+    let splasher;
+    if (args[0] === "splasher" || args[0] === "s") {
+      splasher = args[0];
+    }
+    
+    if (splasher && args.length < 2) {
+      return bot.reply(sender, `When querying a Splasher rule, you must provide a number (1-5).`, VerbosityLevel.Reduced);
+    }
+    
+    let rule;
+    if (!splasher) {
+      if (!args[0]) {
+        rule = bot.utils.rulesList["1"];
+      } else {
+        rule = bot.utils.rulesList[args[0]];
+      }
+    } else {
+      rule = bot.utils.splasherRulesList[args[1]];
+    }
+
+    let ruleNum;
+    if (!splasher) {
+      ruleNum = Object.keys(bot.utils.rulesList).find(
+        (key) => bot.utils.rulesList[key] === rule,
+      );
+    } else {
+      ruleNum = Object.keys(bot.utils.splasherRulesList).find(
+        (key) => bot.utils.splasherRulesList[key] === rule,
+      );
+    }
 
     // TODO: update rules (both data & usage system/mechanism)
     // bot.chat("/pc --- Bingo Brewers Rules (Outdated)---");
-    bot.chat("/pc --- Bingo Brewers Rules ---", VerbosityLevel.Minimal);
-    await bot.utils.delay(bot.utils.minMsgDelay);
-    bot.chat(`/pc Rule ${ruleNum}: ${rule}`, VerbosityLevel.Minimal);
+    if (!splasher) {
+      bot.chat("/pc --- Bingo Brewers Rules ---", VerbosityLevel.Minimal);
+      await bot.utils.delay(bot.utils.minMsgDelay);
+      bot.chat(`/pc Rule ${ruleNum}: ${rule}`, VerbosityLevel.Minimal);
+    } else {
+      return bot.reply(sender, `Splasher Rule ${ruleNum}: ${rule}`, VerbosityLevel.Minimal);
+    }
   },
 };
